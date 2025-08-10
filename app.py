b90616546f104c216928ef75dd8780c2b7cb6eb5
selfnitinpandey <selfnitinpandey.com>
2025-08-10 11:34:12 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.context_processor
+def inject_year():
+    return {'current_year': datetime.now().year}
+
+@app.route("/")
+def home():
+    return render_template("home.html", title="Home | Kings Rugs")
+
+@app.route("/products")
+def products():
+    return render_template("products.html", title="Our Products | Kings Rugs")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About Us | Kings Rugs")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", title="Contact Us | Kings Rugs")
+
+if __name__ == "__main__":
+    app.run(debug=True)
